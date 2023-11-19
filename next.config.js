796dbd4244ca6c8c9a051c993852813304d3c756
Wang Ziyi <mint.wzy@gmail.com>
2023-11-19 10:42:22 +0800

    try to fix the link

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
 })
@@ -5,7 +7,9 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = withMDX({
     pageExtensions: ['ts', 'tsx', 'mdx'],
-    output: "export"
+    output: "export",
+    basePath: ghPages? '/blog-nextjs/' : '',
+    assetPrefix: ghPages ? '/blog-nextjs/' : '',
 })
 
 
